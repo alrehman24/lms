1fa470ed15527c2ba9564a8e148e8c4e24862eba
Hadi <alrehman24@gmail.com>
2025-06-29 21:01:58 +0500

    working on login with vue

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,9 +1,32 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('front_assets/img/favicon.png') }}">
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&amp;display=fallback">
+
+    <link rel="stylesheet" href="admincss/plugins/fontawesome-free/css/all.min.css">
+
+    <link rel="stylesheet" href="admincss/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+
+    <link rel="stylesheet" href="admincss/dist/css/adminlte.min2167.css?v=3.2.0">
+
+
 </head>
+
 <body>
-    <div id="app"></div>
+    @vite('resources/css/app.css')
+    <div id = "app"> </div>
+    @vite('resources/js/app.js')
 </body>
+
+
+
 </html>
